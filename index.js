0467f0dc2108eaebc4eaad11caa15022275d6fc0
Juneau <33215669+voidJuneau@users.noreply.github.com>
2021-05-04 21:29:13 -0400

    Insert hsr values to db

--- index.js
+++ index.js
@@ -14,38 +14,57 @@ express()
   .get('/', (req, res) => {
 
   console.log("process.env.DATABASE_URL: " + process.env.DATABASE_URL)
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-});
+  const pool = new Pool({
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false
+      }
+  });
 
-schema = {
-  agency: "agency_id text,agency_name text,agency_url text,agency_timezone text,agency_lang float,agency_phone text,agency_fare_url text",
-  route: "route_id integer,agency_id text,route_short_name integer,route_long_name text,route_desc text,route_type integer,route_url text,route_color text,route_text_color text",
-  stop_time: "trip_id integer,arrival_time text,departure_time text,stop_id integer,stop_sequence integer,stop_headsign integer,pickup_type integer,drop_off_type integer,shape_dist_traveled float,agency_id text",
-  stop: "stop_id integer,stop_code integer,stop_name text,stop_desc text,stop_lat float,stop_lon float ,zone_id integer,stop_url text,location_type integer,parent_station integer,stop_timezone text,wheelchair_boarding integer,agency_id text",
-  trip: "route_id integer,service_id integer,trip_id integer,trip_headsign text,trip_short_name text,direction_id integer,block_id integer,shape_id integer,wheelchair_accessible integer,bikes_allowed integer,agency_id text"
-}
-
-
-const run = async() => {
-  try {
-    for (const table of Object.keys(schema)) {
-      const client = await pool.connect();
-      const result = await client.query(`CREATE TABLE ${table} (${schema[table]})`);
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      console.log(result)
-      client.release();
-    }
-  } catch (err) {
-    console.error(err);
+  schema = {
+    // agency: "agency_id text,agency_name text,agency_url text,agency_timezone text,agency_lang text,agency_phone text,agency_fare_url text",
+    // route: "route_id integer,agency_id text,route_short_name text,route_long_name text,route_desc text,route_type integer,route_url text,route_color text,route_text_color text",
+    stop_time: "trip_id integer,arrival_time time,departure_time time,stop_id integer,stop_sequence integer,stop_headsign integer,pickup_type integer,drop_off_type integer,shape_dist_traveled float,agency_id text",
+    stop: "stop_id integer,stop_code integer,stop_name text,stop_desc text,stop_lat float,stop_lon float ,zone_id integer,stop_url text,location_type integer,parent_station integer,stop_timezone text,wheelchair_boarding integer,agency_id text",
+    trip: "route_id integer,service_id integer,trip_id integer,trip_headsign text,trip_short_name text,direction_id integer,block_id integer,shape_id integer,wheelchair_accessible integer,bikes_allowed integer,agency_id text"
   }
-}
-run();
 
-res.render('pages/index')
+  const run = async() => {
+    try {
+      const client = await pool.connect();
+      for (const table of Object.keys(schema)) {
+        // Create DB
+        // const result = await client.query(`CREATE TABLE ${table} (${schema[table]})`);
+        // const results = { 'results': (result) ? result.rows : null};
+        // res.render('pages/index', results );
+        // client.release();
+
+        // Insert values
+        let data;
+        try {
+          data = fs.readFileSync(table==="agency"?`hsr/${table}.txt`:`hsr/${table}s.txt`, 'utf8')
+        } catch (err) {
+          console.error(err);
+          return
+        }
+        data = data.split("\n")
+        header = data[0]
+        for (let i = 1; i < data.length; i++) { // ignore the first line (header)
+          if (!data[i]) continue; // for last empty row
+          let line = data[i].split(",").map(a => a.trim() === ""?"Null":`'${a.trim()}'`).join(",") // to allow empty value for integer
+          console.log(header)
+          console.log(line)
+          const result = await client.query(`INSERT INTO ${table}(${header}) VALUES (${line})`);
+          console.log(result)
+        }
+      }
+
+
+    } catch (err) {
+      res.send("Error " + err);
+      console.log(err);
+    }
+  }
+  run();
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
