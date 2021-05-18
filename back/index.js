63fe60d27a0c0e70091eac5ff4c97f6115c5b9b5
Juneau Lim <juneaulim@outlook.com>
2021-05-17 21:14:55 -0400

    change path to build

--- back/index.js
+++ back/index.js
@@ -11,10 +11,10 @@ app.use('/', routes);
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, 'back/build')));
+  app.use(express.static(path.join(__dirname, 'front/build')));
   // Handle React routing, return all requests to React app
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'back/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'front/build', 'index.html'));
   });
 }
 
