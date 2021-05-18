8a4ff0f381c9f007f8fa5244ddc4cbdb22a30ec2
Juneau Lim <jungwon.limm@gmail.com>
2021-05-18 19:56:49 -0400

    change structure

--- back/index.js
+++ back/index.js
@@ -1,4 +1,3 @@
-// Bring in our dependencies
 const express = require('express');
 const routes = require('./routes');
 const dotenv = require('dotenv');
@@ -14,16 +13,6 @@ app.use(express.static(buildPath));
 //  Connect all our routes to our application
 app.use('/', routes);
 
-// if (process.env.NODE_ENV === 'production') {
-//   // Serve any static files
-//   app.use(express.static(path.join(__dirname, 'front/build')));
-//   // Handle React routing, return all requests to React app
-//   app.get('*', function(req, res) {
-//     res.sendFile(path.join(__dirname, 'front/build', 'index.html'));
-//   });
-// }
-
-// Turn on that server!
 const server = app.listen(PORT, () => {
   console.log(`Listening on ${ PORT }`);
 });
