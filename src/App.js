7cd59856084da1161f7edafe66a8d331995fa990
Juneau Lim <jungwon.limm@gmail.com>
2021-05-18 21:13:46 -0400

    Setup navbar

--- src/App.js
+++ src/App.js
@@ -1,52 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
+
+import Navbar from './components/ui/Navbar';
 
 import './App.css';
 
-class App extends Component {
-  state = {
-    response: '',
-    post: '',
-    responseToPost: '',
-  };
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-  callApi = async () => {
-    const response = await fetch('/arrivals/hsr/stop/1499/route/4421');
-    const body = await response.json();
-    if (response.status !== 200) return Error(body.message);
-    return body;
-  };
-  handleSubmit = async e => {
-    e.preventDefault();
-    const response = await fetch('/arrivals/hsr/stop/1499/route/4421', {
-      method: 'GET'
-    });
-    const body = await response.text();
-    this.setState({ responseToPost: body });
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <p>{this.state.response}</p>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <strong>Post to Server:</strong>
-          </p>
-          <input
-            type="text"
-            value={this.state.post}
-            onChange={e => this.setState({ post: e.target.value })}
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <p>{this.state.responseToPost}</p>
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>content</div>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/components/ui/Navbar.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { AppBar, Button, Container, IconButton, List, ListItem, ListItemText, makeStyles, 
+  Toolbar, Typography } from '@material-ui/core';
+import { Home, MenuIcon } from '@material-ui/icons'; 
+
+const navLinks = [
+    { title: `about us`, path: `/about-us` },
+    { title: `product`, path: `/product` },
+    { title: `blog`, path: `/blog` },
+    { title: `contact`, path: `/contact` },
+    { title: `faq`, path: `/faq` },
+  ]
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Container className={classes.navbarDisplayFlex}>
+            <IconButton edge="start" color="inherit" aria-label="home">
+              <Home fontSize="large" />
+            </IconButton>
+            <Typography variant="h6" className={classes.title}>
+              Show My Bus
+            </Typography>
+            <List 
+              component="nav" 
+              aria-labelledby="main navigation"
+              className={classes.navDisplayFlex} >
+              {navLinks.map(({ title, path }) => (
+                <a href={path} key={title} className={classes.linkA}>
+                  <ListItem button>
+                    <ListItemText primary={title} className={classes.linkText} />
+                  </ListItem>
+                </a>
+              ))}
+            </List>
+          </Container>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  navbarDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`
+  },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    alignSelf: "center"
+  },
+  navDisplayFlex: {
+    display: `flex`,
+    justifyContent: `space-between`
+  },
+  linkText: {
+    textDecoration: `none`,
+    textTransform: `uppercase`,
+    color: `white`
+  },
+  linkA: {
+    textDecoration: "none"
+  }
+}));
