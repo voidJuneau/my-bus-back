86b840ab68d2c6638eed0b3532611223857a8667
Juneau Lim <jungwon.limm@gmail.com>
2021-05-20 19:24:59 -0400

    Styling default menu and adding theme

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'
-import { Container, Grid } from '@material-ui/core';
+import { Container, createMuiTheme, Grid, MuiThemeProvider } from '@material-ui/core';
+import blue from '@material-ui/core/colors/blue';
+import amber from '@material-ui/core/colors/amber';
 
 import './App.css';
 import Side from './components/ui/Side';
 import Map from './components/Map';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: blue["500"]
+    },
+    secondary: {
+      main: amber["A200"]
+    }
+  }
+});
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Container disableGutters>
-        <Grid container>
-          <Grid item xs={12} sm={12} md={4}>
-            <Side />
+      <MuiThemeProvider theme={theme}>
+        <Container disableGutters>
+          <Grid container>
+            <Grid item xs={12} sm={12} md={4}>
+              <Side />
+            </Grid>
+            <Grid item md={8}>
+              <Map zoom={10} center={{lat:40, lng:-80}} markers={[]}/>
+            </Grid>
           </Grid>
-          <Grid item md={8}>
-            <Map zoom={10} center={{lat:40, lng:-80}} markers={[]}/>
-          </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </MuiThemeProvider>
     </BrowserRouter>
   )
 }
