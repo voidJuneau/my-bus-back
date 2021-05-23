9e5b2246603ffab4349b28906da377cf6eacf867
Juneau Lim <jungwon.limm@gmail.com>
2021-05-22 23:53:54 -0400

    Set menu icons on box item

--- src/components/pages/lines/LineBox.js
+++ src/components/pages/lines/LineBox.js
@@ -2,18 +2,6 @@ import { Box, Grid, makeStyles } from "@material-ui/core";
 import React from "react";
 import BoxMenu from "../../ui/BoxMenu";
 
-const a = {
-  "route_id": "4349",
-  "agency_id": "HSR",
-  "route_short_name": "01",
-  "route_long_name": "KING",
-  "route_desc": null,
-  "route_type": 3,
-  "route_url": null,
-  "route_color": "0093DD",
-  "route_text_color": "000000"
-}
-
 const hsrLogo = require("../../../images/hsr.png");
 const goLogo = require("../../../images/go.svg");
 const burlLogo = require("../../../images/burl.png");
@@ -36,14 +24,14 @@ export default function LineBox({ line }) {
   }
 
   return (
-    <Grid container className={classes.lineBox}>
-      <Grid item>
+    <Grid container className={classes.lineBox} alignItems="center">
+      <Grid item className={classes.listBoxItem}>
         <img src={logo.default} alt="Agency logo" width="32px" />
       </Grid>
-      <Grid item>
+      <Grid item className={classes.listBoxItem}>
         {line.route_short_name}
       </Grid>
-      <Grid item>
+      <Grid item className={classes.listBoxItem}>
         {line.route_long_name}
       </Grid>
       <BoxMenu />
@@ -53,6 +41,9 @@ export default function LineBox({ line }) {
 
 const useStyle = makeStyles(theme => ({
   lineBox: {
-    border: "1px solid black"
+    border: "1px solid black",
+  },
+  listBoxItem: {
+    padding: "8px"
   }
 }))
--- src/components/ui/BoxMenu.js
+++ src/components/ui/BoxMenu.js
@@ -1,15 +1,33 @@
 import React from "react";
 import { Grid, makeStyles } from "@material-ui/core";
 import StarOutlineOutlinedIcon from '@material-ui/icons/StarOutlineOutlined';
+import KeyboardOutlinedIcon from '@material-ui/icons/KeyboardOutlined';
+import RoomIcon from '@material-ui/icons/Room';
+import MapOutlinedIcon from '@material-ui/icons/MapOutlined';
 
 export default function BoxMenu({ data }) {
   const classes = useStyle();
 
   return (
     <Grid item className={classes.boxMenu}>
-      <Grid container>
-        <Grid item>
+      <Grid container direction="column">
+        {/* favourate for all */}
+        <Grid item container justify="flex-end">
           <StarOutlineOutlinedIcon />
+        </Grid>
+        <Grid item container xs direction="row">
+          {/* discussion - for line */}
+          <Grid item xs>
+            <KeyboardOutlinedIcon />
+          </Grid>
+          {/* stops - for line */}
+          <Grid item>
+            <RoomIcon />
+          </Grid>
+          {/* on map - for line, stop */}
+          <Grid>
+            <MapOutlinedIcon />
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
@@ -18,6 +36,7 @@ export default function BoxMenu({ data }) {
 
 const useStyle = makeStyles(theme => ({
   boxMenu: {
-    marginLeft: "auto"
+    marginLeft: "auto",
+    padding: "4px"
   }
 }))
