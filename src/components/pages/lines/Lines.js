79c437c90c344398914b70e1be6cca798f5380c1
Juneau Lim <jungwon.limm@gmail.com>
2021-05-24 20:25:00 -0400

    Add pagenation to lines

--- src/components/pages/lines/Lines.js
+++ src/components/pages/lines/Lines.js
@@ -1,5 +1,8 @@
-import React from "react";
-import { Container } from "@material-ui/core";
+import React, { useState, useEffect } from "react";
+import { useLocation, withRouter } from "react-router-dom";
+import { Container, makeStyles } from "@material-ui/core";
+import Pagination from '@material-ui/lab/Pagination';
+
 import LineCard from "./LineCard";
 
 const data = [{
@@ -25,10 +28,41 @@ const data = [{
   "route_text_color": "FFFFFF"
   },]
 
-export default function Lines() {
+const getLines = async (limit, page) => {
+  const response = await fetch(`/api/lines?limit=${limit}&page=${page}`)
+}
+
+const Lines = () => {
+  const classes = useStyles();
+  const limit = 5;
+  const [ page, setPage ] = useState(Math.floor(42/limit));
+  const [ lines, setLines ] = useState()
+  
+  const handleChange = (event, value) => {
+    setPage(value);
+  }
+
+  useEffect(() => {
+
+  }, [page])
+  
   return (
     <Container>
-      {data.map(d => (<LineCard line={d} />))}
+      {data.map(d => (<LineCard line={d} key={d.route_id}/>))}
+      <div>page: {page}</div>
+      <div className={classes.root}>
+        <Pagination count={totalPages} onChange={handleChange} />
+      </div>
     </Container>
   )
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
+
+export default withRouter(Lines);
