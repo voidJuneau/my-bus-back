026e0a71529c004ec59da73935c65a0dcf216c20
Juneau Lim <jungwon.limm@gmail.com>
2021-06-07 21:32:15 -0400

    Add pagenation on single line page

--- src/components/pages/line/Line.js
+++ src/components/pages/line/Line.js
@@ -1,27 +1,54 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from 'react-router-dom'
-import { Card, Container, Grid } from "@material-ui/core";
+import { Container, Grid, makeStyles } from "@material-ui/core";
+import Pagination from '@material-ui/lab/Pagination';
 
 import LineHeaderCard from "./LineHeaderCard";
 import StopMarker from "../../map/StopMarker";
 import StopListCard from "../stops/StopListCard";
 
+const getCount = async (aId, lId) => {
+  return await (await fetch(`/api/stops/${aId}/route/${lId}/count`)).json();
+}
+
+const getStops = async (aId, lId, limit, page) => {
+  return await (await fetch(`/api/stops/${aId}/route/${lId}?limit=${limit}&page=${page}`)).json();
+}
+
 export default function Stop({ setCenter, setMarkers, setIsMap }) {
+  const classes = useStyles();
   const { aId, lId } = useParams();
+  const [ page, setPage ] = useState(1);
   const [ line, setLine ] = useState({});
   const [ stops, setStops ] = useState([]);
+  const [ totalPages, setTotalPages ] = useState(0);
+  const limit = 5;
 
   useEffect(() => {
-    // Load line info
+    // Get stops on that line
+    getStops(aId, lId, limit, page)
+    .then(stops => setStops(stops));
+  }, [page])
+
+  useEffect(() => {
+    // Get line info
     fetch(`/api/lines/${aId}/route/${lId}`)
     .then(res => res.json())
     .then(data => setLine(data));
     
-    // Load stops on that line
-    fetch(`/api/stops/${aId}/route/${lId}`)
-    .then(res => res.json())
-    .then(data => setStops(data));
+    // Get stops on that line
+    // fetch(`/api/stops/${aId}/route/${lId}`)
+    // .then(res => res.json())
+    // .then(data => setStops(data));
+
+    // Get count of stops for that line
+    getCount(aId, lId)
+    .then(res => setTotalPages(Math.ceil(res / limit)));
   }, [])
+  
+  const handleChange = (event, value) => {
+    setPage(value);
+  }
 
   return (
     <Container>
@@ -35,6 +62,17 @@ export default function Stop({ setCenter, setMarkers, setIsMap }) {
           </Grid>
         ))}
       </Grid>
+      <div className={classes.root}>
+        <Pagination count={totalPages} onChange={handleChange} />
+      </div>
     </Container>
   )
 }
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      marginTop: theme.spacing(2),
+    },
+  },
+}));
